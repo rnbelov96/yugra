fbb51e305ba893239e80f03c854c7509522d7a70
Belov Roman <rnbelov96@gmail.com>
2022-01-20 05:48:58 +0300

    Refactoring animation

--- src/ts/animation.ts
+++ src/ts/animation.ts
@@ -4,19 +4,24 @@ const sectionEl = document.querySelector('.stats');
 const iconImgElList = document.querySelectorAll('.stats__icon-img');
 const iconTextElList = document.querySelectorAll('.stats__icon-text');
 
-window.addEventListener('scroll', () => {
-  let breakpoint = 0;
-  const screenWidth = document.documentElement.scrollWidth;
+let breakpoint = 0;
+const screenWidth = document.documentElement.scrollWidth;
 
-  if (screenWidth <= 650) {
-    breakpoint = -531;
-  }
-  if (screenWidth > 650 && screenWidth <= 800) {
-    breakpoint = -240;
-  }
-  if (screenWidth > 800 && screenWidth < 1920) {
-    breakpoint = -327;
-  }
+if (screenWidth <= 650) {
+  breakpoint = -531;
+}
+if (screenWidth > 650 && screenWidth <= 800) {
+  breakpoint = -240;
+}
+if (screenWidth > 800 && screenWidth <= 1400) {
+  breakpoint = -327;
+}
+
+if (screenWidth > 1400 && screenWidth < 1920) {
+  breakpoint = -100;
+}
+
+const launchAnimation = () => {
   if (sectionEl && sectionEl.getBoundingClientRect().top < breakpoint) {
     iconImgElList.forEach((iconImgEl, i) => {
       setTimeout(() => {
@@ -28,5 +33,8 @@ window.addEventListener('scroll', () => {
         iconTextEl.classList.add('stats__icon-text_shown');
       }, i * 250);
     });
+    window.removeEventListener('scroll', launchAnimation);
   }
-});
+};
+
+window.addEventListener('scroll', launchAnimation);
